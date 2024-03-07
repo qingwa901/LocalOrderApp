7cfb0aa0ffddaae4642b6045ffb16940bfbf3f80
qzt <qingwa901@gmail.com>
2024-03-07 20:06:38 +0000

    correct receipt formate

--- QtApp/receiptPanel.py
+++ QtApp/receiptPanel.py
@@ -32,6 +32,59 @@ class Receipt(QtWidgets.QFrame):
 
         QtCore.QMetaObject.connectSlotsByName(self)
 
+    def LoadHtml2(self, TableInfo: TableInfoStore):
+        receiptform = ('''<html><body><style type="text/css">.receipt_main {display: grid;width = 100%;
+                                grid-template-columns: 1fr 1fr;align-items: center;}.itemname{flex-wrap: wrap;}
+                                .itemprice{margin-right:0;margin-left: auto;}</style>
+                                <div id="receipt">
+                                <div id="receipt_head" style="text-align: center;">
+                                <div id="icon"><img src="file://{Path_To_LOGO}" style="width:50px;height:50px;"></div>
+                                <div id="address">{ADDRESS}</div>
+                                </div><hr>
+                                <div id="receipt_info">VAT No: {VATNO}<br>Date: {TIME}<br>#ORDER: {ORDERID}<br>#Table: {TABLE_NUMBER}
+                                </div></div><hr>'''.replace('{Path_To_LOGO}', 'D:/Code/web_python/App/img/Login.png')
+                       .replace('{ADDRESS}', 'Address')
+                       .replace('{VATNO}', '')
+                       .replace('{TIME}', TableInfo.EndTime)
+                       .replace('{ORDERID}', str(TableInfo.OrderID))
+                       .replace('{TABLE_NUMBER}', str(TableInfo.TableID)))
+        Total = TableInfo.GetTotalAmount()
+        receiptform += "<table width='100%'>"
+        for OrderID in TableInfo.Orders:
+            Order = TableInfo.Orders[OrderID]
+            receiptform += ('''<tr><td style="flex-wrap: wrap;">{QTY} x {NAME} {NOTE}</td>
+            <td style="text-align: right;">£{PRICE}</td>'''
+                            .replace('{QTY}', str(int(Order.Qty))).replace('{NAME}', Order.NameEN)
+                            .replace('{NOTE}', Order.Note).replace('{PRICE}', str(round(Order.UnitPrice, 2))))
+        receiptform += "</table><hr><table width='100%'>"
+        receiptform += ('''<tr><td style="flex-wrap: wrap;">SUBTOTAL</td>
+                            <td style="text-align: right;">£{TOTAL}</td></tr>'''
+                        .replace('{TOTAL}', str(round(Total, 2))))
+        if TableInfo.ServiceCharge != 0:
+            receiptform += ('''<tr><td style="flex-wrap: wrap;">{SERVICE_CHARGE_PERCENT}% Service Charge</td>
+                                    <td style="text-align: right;">
+                                    £{SERVICE_CHARGE_AMOUNT}</td></tr>'''
+                            .replace('{SERVICE_CHARGE_PERCENT}', str(round(TableInfo.ServiceCharge, 1)))
+                            .replace('{SERVICE_CHARGE_AMOUNT}', str(round(Total * TableInfo.ServiceCharge, 2))))
+            Total = Total * (1 + TableInfo.ServiceCharge)
+        DiscountPercent = round(TableInfo.Discount)
+        if DiscountPercent > 0:
+            receiptform += ('''<tr><td style="flex-wrap: wrap;">{DISCOUNT_PERCENT}%DISCOUNT</td>
+                                <td style="text-align: right;">£{DISCOUNT_AMOUNT}}</td></tr>'''
+                            .replace('{DISCOUNT_PERCENT}', str(round(TableInfo.Discount)))
+                            .replace('{DISCOUNT_AMOUNT}', str(round(Total * TableInfo.Discount, 2))))
+            Total = Total * (1 - DiscountPercent)
+
+        receiptform += '''</table><hr><table width='100%'>
+                                <tr><td style="flex-wrap: wrap;">12.5% VAT included</td></tr>
+                                <tr><td style="flex-wrap: wrap;">TOTAL</td>
+                                <td style="text-align: right;">£{TOTAL}</td></tr></table><hr>
+                                <div id="receipt_foot" style="text-align: center;">
+                                Thanks for visiting!<br>You can find us on instagram<br> @usagi_animemaid_cafe</div>
+                                </div></body></html>
+                                '''.replace('{TOTAL}', str(round(Total, 2)))
+        self.HTML = receiptform
+        return receiptform
     def LoadHtml(self, TableInfo: TableInfoStore):
         receiptform = ('''<html><body><style type="text/css">.receipt_main {display: grid;width = 100%;
                                 grid-template-columns: 1fr 1fr;align-items: center;}.itemname{flex-wrap: wrap;}
@@ -52,8 +105,7 @@ class Receipt(QtWidgets.QFrame):
         for OrderID in TableInfo.Orders:
             Order = TableInfo.Orders[OrderID]
 
-            receiptform += ('''<div id="item" class="receipt_main" style="display: grid;width = 100%;
-                                grid-template-columns: 1fr 1fr;align-items: center;">
+            receiptform += ('''<div id="item" class="receipt_main" style="flex: 1;width = 100%;align-items: center;">
                                     <div class="itemname" style="flex-wrap: wrap;">{QTY} x {NAME} {NOTE}</div>
                                     <div class="itemprice" style="margin-right:0;margin-left: auto;">£{PRICE}</div>
                                     </div>'''.replace('{QTY}', str(int(Order.Qty))).replace('{NAME}', Order.NameEN)
@@ -99,7 +151,7 @@ class Receipt(QtWidgets.QFrame):
         return receiptform
 
     def LoadTable(self, TableInfo: TableInfoStore):
-        receiptform = self.LoadHtml(TableInfo)
+        receiptform = self.LoadHtml2(TableInfo)
         self.web_view.setHtml(receiptform)
 
     def print_me2(self):
